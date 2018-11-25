a2b5a65275551165f5bd7afcec37087b505c453a
Gaetan <ligno19@gmail.com>
2018-11-24 19:46:33 -0500

    Update Game controler

---
+++ JamNation2018_19_2/Assets/Scripts/RythmedObject/Wheel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wheel : MonoBehaviour, BasicRythmed {
+
+
+    public void Tick() {
+        // TODO Call anim
+
+    }
+
+    void Awake() {
+        GameController.instance.AddBasicRythmed(this);
+    }
+
+}
